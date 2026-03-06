81a6b32eadee1f031550cd291befc20b40381c08
lsr54199251 <lsr54199251@gmail.com>
2026-03-06 14:42:52 +0900

    skfk thro doq dhkstjd

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,22 @@
 import 'package:flutter/material.dart';
-import 'card_page.dart';
-
+import 'country_list_page.dart';  // 나라 목록 파일
 
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: InputFormPage(),
-    );
-  }
-}
-
-class InputFormPage extends StatefulWidget {
-  @override
-  State<InputFormPage> createState() => _InputFormPageState();
-}
-
-class _InputFormPageState extends State<InputFormPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('명함정보입력'),
-        backgroundColor: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: Colors.indigo,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.indigo,
+          foregroundColor: Colors.white, // 글자/ 아이콘 색
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Text(
-              '명함에 들어갈 정보를 입력하세요',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-
-            TextField(
-              decoration: InputDecoration(
-                labelText: '이름',
-                hintText: '이름을 입력하세요',
-                prefixIcon: Icon(Icons.person),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: '직업',
-                hintText: '직업',
-                prefixIcon: Icon(Icons.work),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: '이메일',
-                hintText: '이메일',
-                prefixIcon: Icon(Icons.email),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: '전화번호',
-                hintText: '전화번호',
-                prefixIcon: Icon(Icons.phone),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => CardPage()),
-                );
-              },
-              child: Text('명함만들기'),
-            ),
-          ],
-        ),
       ),
+      home: CountryListPage(),  // 첫 화면
     );
   }
 }
